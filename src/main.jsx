000cd2cdf308e5726c0ccfa976a3eeb29d417182
sourovroy999 <sourovroy3050@gmail.com>
2025-06-26 03:03:53 +0600

    fix: toast was showing four times. fix it by remove strict mode in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,12 @@ import { Toaster } from 'react-hot-toast'
 
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  
     <AuthProvider>
 
     <RouterProvider router={router}/>
 
     <Toaster/>
     </AuthProvider>
-  </StrictMode>,
+  
 )
